8298552426fa841b1df39029a4fa7b0dbe546dd1
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-02-14 20:17:21 +0100

    Rewrite conversion to Makefile.PL

--- convert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-KSY_FILE=dwg_ac1004.ksy
-OUTPUT_PM_FILE=AC1004.pm
-
-ksc -t perl $KSY_FILE
-mv DwgAc1004.pm $OUTPUT_PM_FILE
-sed -i s/DwgAc1004/CAD::Format::DWG::AC1004/g $OUTPUT_PM_FILE

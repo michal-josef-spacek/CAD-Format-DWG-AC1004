2b3764feb17bb49099cd5e86fc307ac1977c724d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-03-21 10:33:34 +0100

    Improve block

--- AC1004.pm
+++ AC1004.pm
@@ -448,6 +448,86 @@ sub xxx {
 }
 
 ########################################################################
+package CAD::Format::DWG::AC1004::BlockFlag;
+
+our @ISA = 'IO::KaitaiStruct::Struct';
+
+sub from_file {
+    my ($class, $filename) = @_;
+    my $fd;
+
+    open($fd, '<', $filename) or return undef;
+    binmode($fd);
+    return new($class, IO::KaitaiStruct::Stream->new($fd));
+}
+
+sub new {
+    my ($class, $_io, $_parent, $_root) = @_;
+    my $self = IO::KaitaiStruct::Struct->new($_io);
+
+    bless $self, $class;
+    $self->{_parent} = $_parent;
+    $self->{_root} = $_root || $self;;
+
+    $self->_read();
+
+    return $self;
+}
+
+sub _read {
+    my ($self) = @_;
+
+    $self->{flag1} = $self->{_io}->read_bits_int_be(1);
+    $self->{flag2} = $self->{_io}->read_bits_int_be(1);
+    $self->{flag3} = $self->{_io}->read_bits_int_be(1);
+    $self->{flag4} = $self->{_io}->read_bits_int_be(1);
+    $self->{flag5} = $self->{_io}->read_bits_int_be(1);
+    $self->{flag6} = $self->{_io}->read_bits_int_be(1);
+    $self->{flag7} = $self->{_io}->read_bits_int_be(1);
+    $self->{flag8} = $self->{_io}->read_bits_int_be(1);
+}
+
+sub flag1 {
+    my ($self) = @_;
+    return $self->{flag1};
+}
+
+sub flag2 {
+    my ($self) = @_;
+    return $self->{flag2};
+}
+
+sub flag3 {
+    my ($self) = @_;
+    return $self->{flag3};
+}
+
+sub flag4 {
+    my ($self) = @_;
+    return $self->{flag4};
+}
+
+sub flag5 {
+    my ($self) = @_;
+    return $self->{flag5};
+}
+
+sub flag6 {
+    my ($self) = @_;
+    return $self->{flag6};
+}
+
+sub flag7 {
+    my ($self) = @_;
+    return $self->{flag7};
+}
+
+sub flag8 {
+    my ($self) = @_;
+    return $self->{flag8};
+}
+
+########################################################################
 package CAD::Format::DWG::AC1004::EntityLine3d;
 
 our @ISA = 'IO::KaitaiStruct::Struct';
@@ -1780,9 +1860,8 @@ sub new {
 sub _read {
     my ($self) = @_;
 
-    $self->{unknown1} = $self->{_io}->read_s1();
-    $self->{block_name} = Encode::decode("ASCII", IO::KaitaiStruct::Stream::bytes_terminate($self->{_io}->read_bytes(31), 0, 0));
-    $self->{u1} = $self->{_io}->read_s1();
+    $self->{flag} = CAD::Format::DWG::AC1004::BlockFlag->new($self->{_io}, $self, $self->{_root});
+    $self->{block_name} = Encode::decode("ASCII", IO::KaitaiStruct::Stream::bytes_terminate($self->{_io}->read_bytes(32), 0, 0));
     $self->{u2} = $self->{_io}->read_s1();
     $self->{u3} = $self->{_io}->read_s1();
     $self->{u4} = $self->{_io}->read_s1();
@@ -1790,19 +1869,14 @@ sub _read {
     $self->{u6} = $self->{_io}->read_s1();
 }
 
-sub unknown1 {
+sub flag {
     my ($self) = @_;
-    return $self->{unknown1};
+    return $self->{flag};
 }
 
 sub block_name {
     my ($self) = @_;
     return $self->{block_name};
-}
-
-sub u1 {
-    my ($self) = @_;
-    return $self->{u1};
 }
 
 sub u2 {
--- dwg_ac1004.ksy
+++ dwg_ac1004.ksy
@@ -50,15 +50,15 @@ seq:
 types:
   block:
     seq:
-      - id: unknown1
-        type: s1
+      - id: flag
+        type: block_flag
+        doc: BLOCK/70
       - id: block_name
-        size: 31
+        size: 32
         type: str
         encoding: ASCII
         terminator: 0x00
-      - id: u1
-        type: s1
+        doc: BLOCK/2
       - id: u2
         type: s1
       - id: u3
@@ -69,6 +69,24 @@ types:
         type: s1
       - id: u6
         type: s1
+  block_flag:
+    seq:
+      - id: flag1
+        type: b1
+      - id: flag2
+        type: b1
+      - id: flag3
+        type: b1
+      - id: flag4
+        type: b1
+      - id: flag5
+        type: b1
+      - id: flag6
+        type: b1
+      - id: flag7
+        type: b1
+      - id: flag8
+        type: b1
   header:
     seq:
       - id: magic

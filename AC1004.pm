f96a1f3b2ccb6d178a5258ef99f10fd3230a8b66
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-01-23 03:18:49 +0100

    Identify aspect_ratio

--- AC1004.pm
+++ AC1004.pm
@@ -3557,7 +3557,7 @@ sub _read {
     $self->{unknown7b} = $self->{_io}->read_bytes(2);
     $self->{unknown7c} = $self->{_io}->read_bytes(2);
     $self->{unknown7d} = $self->{_io}->read_bytes(2);
-    $self->{unknown8} = $self->{_io}->read_f8le();
+    $self->{aspect_ratio} = $self->{_io}->read_f8le();
     $self->{linear_units_format} = $self->{_io}->read_s2le();
     $self->{linear_units_precision} = $self->{_io}->read_s2le();
     $self->{axis} = $self->{_io}->read_s2le();
@@ -3837,9 +3837,9 @@ sub unknown7d {
     return $self->{unknown7d};
 }
 
-sub unknown8 {
+sub aspect_ratio {
     my ($self) = @_;
-    return $self->{unknown8};
+    return $self->{aspect_ratio};
 }
 
 sub linear_units_format {
